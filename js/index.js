1609b6c17fe41b26ba862c36370e91da92e65470
Zain <zainjaved965@gmail.com>
2023-01-24 21:11:10 +0500

    added form authentication

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ let toggleMenu = document.querySelector("#show")
 let sideMenu =  document.querySelector(".nav-menu")
 let main = document.querySelector(".main")
 let navBar = document.querySelector(".nav")
-
+let email = document.getElementById("email")
+let warning = document.getElementById("warning")
+let gmail = document.getElementById("gmail")
+let password = document.getElementById("password")
 function showMenu() {
     sideMenu.classList.add("active")
     main.classList.add("side-effect")
@@ -15,6 +18,30 @@ function hideMenu(){
 
 }
 
-function submit(){
-        window.location.href="http://programminghead.com";  
+function emailSubmit(){
+    console.log("button clicked")
+    if (email.value == "" ){
+        email.classList.add("wrong")
+        warning.classList.add("warning-enabled")
+    }
+
+    else if (email.value.includes("@")){
+        console.log("email valid")
+        window.location.href = "/password.html"
+    }
+    else {
+        email.classList.add("wrong")
+        warning.classList.add("warning-enabled")
+    }
+
+}
+
+function passSubmit(){
+    if (password.value.length <  8 ){
+        password.classList.add("wrong")
+        warning.classList.add("warning-enabled")
+    }
+    else {
+        window.location.href = "/index.html"
+    }
 }
